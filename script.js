4b44bedbbd8c1616af3c155df71f3314b6a09fc5
Tushar Chaudhary <tushar.chaudhary@techangouts.com>
2024-03-27 10:40:12 +0530

    add template

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
 #!/usr/bin/env node
+const { execSync } = require("child_process");
 
-console.log("This is post init script");
+const installDependencies = () => {
+  const dependencies = [
+    "@react-navigation/native",
+    "@react-navigation/native-stack",
+    "axios",
+    "react-native-safe-area-context",
+    "react-native-screens",
+    "react-native-svg",
+    "react-native-reanimated",
+  ];
+
+  const devDependencies = [
+    "@testing-library/jest-native",
+    "@testing-library/react-native",
+    "babel-plugin-module-resolver",
+    "cz-conventional-changelog",
+    "eslint-plugin-import",
+    "lint-staged",
+    "msw",
+    "ts-node",
+    "react-native-dotenv",
+  ];
+
+  console.log("\n");
+
+  console.log("@tushu4212/rntemplate initialized with success! 🚀\n");
+
+  console.log("Installing dependencies... 🛠️\n");
+  execSync(`yarn add ${dependencies.join(" ")}`, { stdio: "inherit" });
+  console.log("Dependencies installed successfully. 🚀\n");
+
+  console.log("Installing dev dependencies... 🛠️\n");
+  execSync(`yarn add ${devDependencies.join(" ")} -D`, { stdio: "inherit" });
+  console.log("Dev dependencies installed successfully.🚀\n");
+};
+
+const main = () => {
+  execSync("git init", { stdio: "inherit" });
+  installDependencies();
+};
+
+new Promise((resolve) => {
+  main();
+  resolve();
+})
+  .then(() => {
+    console.log(
+      "- 🎉  Congrats! Your project is ready with @tushu4212/rntemplate! 🎉\n"
+    );
+  })
+  .catch((error) => {
+    console.error(`🚨 An error occurred with post init script: ${error}`);
+    throw new Error();
+  });
---
+++ template/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Text} from 'react-native'
+
+
+export const App = () => {
+  return (
+    <View style={{flex:1,alignItems:'center',justifyContext:'center'}}>
+      <Text>Hello from template</Text>
+   </View>
+  );
+};
